dee0dbe701f2635def765a67bbf68cbda75d9eed
ammar <ammar.ali.haidar.1990@gmail.com>
2023-02-24 18:57:59 +0300

    all-done-fixed

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Footer from "@/features/components/footer/Footer";
 import Layout from "@/features/components/layout/Layout";
 import HomeBanner from "@/features/components/home/HomeBanner";
 import HomeProperties from "@/features/components/homeProperties/HomeProperties";
-import MeatTheTeam from "@/features/components/MeatTheTeam/MeatTheTeam";
+import MeatTheTeam from "@/features/components/meatTheTeam/MeatTheTeam";
 import Partners from "@/features/components/partners/Partners";
 import Testimonials from "@/features/components/testimonials/Testimonials";
 import { getProperty } from "@/lib/axios";
